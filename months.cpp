0f4056fe443e95f39d6437522918190ec82793c2
Ayush kamboj <152530498+Ayushh-k@users.noreply.github.com>
2024-08-28 19:54:30 +0530

    Add files via upload

---
+++ months.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout << "Enter the number of month: ";
+    cin >> n;
+    switch (n)
+    {
+    case 1:
+        cout << "The Month is: January" << endl;
+        break;
+    case 2:
+        cout << "The Month is: February" << endl;
+        break;
+    case 3:
+        cout << "The Month is: March" << endl;
+        break;
+    case 4:
+        cout << "The Month is: April" << endl;
+        break;
+    case 5:
+        cout << "The Month is: May" << endl;
+        break;
+    case 6:
+        cout << "The Month is: June" << endl;
+        break;
+    case 7:
+        cout << "The Month is: July" << endl;
+        break;
+    case 8:
+        cout << "The Month is: August" << endl;
+        break;
+    case 9:
+        cout << "The Month is: September" << endl;
+        break;
+    case 10:
+        cout << "The Month is: October" << endl;
+        break;
+    case 11:
+        cout << "The Month is: November" << endl;
+        break;
+    case 12:
+        cout << "The Month is: December" << endl;
+        break;
+    default:
+        cout << "Invalid input!!!!" << endl;
+    }
+    return 0;
+}
